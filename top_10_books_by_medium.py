a199d0dbc6004b125fd482b13496fab10a57b2a2
pratheek <ms.pratheek1@gmail.com>
2020-01-09 18:08:32 +0530

    first commit

---
+++ top_10_books_by_medium.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 16:38:56 2020
+
+@author: pratms
+"""
+
+import bs4,requests
+finalResult={}
+def scrapingFunction(name,urlName,tagName,className):
+    
+    #url='http://www.moneycontrol.com/nifty/nse/nifty-live'
+    res=requests.get(urlName,verify=False)
+    res.raise_for_status()
+    soup=bs4.BeautifulSoup(res.text,'lxml')
+    value=soup.find_all(tagName,attrs={"class":className})
+    sensex=value[0].getText()
