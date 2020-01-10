17af4d5ca7275f9697cc88fba2f5a914d7190b6c
pratheek <ms.pratheek1@gmail.com>
2020-01-10 18:16:19 +0530

    rating script program started

--- finding_book_rating.py
+++ finding_book_rating.py
@@ -8,8 +8,18 @@ import time
 start = time.time()
 import bs4,requests
 import openpyxl
-path=r"C:\Users\pratms\pythonprojects\webscrapping\webscrapping_medium_top_10\excel_doc.xlsx"
+path=r"C:\Users\pratms\pythonprojects\webscrapping\webscrapping_medium_top_10\excel_doc1.xlsx"
 
 wb_obj=openpyxl.load_workbook(path)
 sheet_obj=wb_obj.active
 m_row=sheet_obj.max_row
+print("rows="+str(m_row))
+for i in range(1,m_row+1):
+    cell_obj=sheet_obj.cell(row=i,column=1)
+    cell_value=cell_obj.value
+    print(cell_value)
+    
+    
+    
+end = time.time()
+print("time taken by program is:"+str(end - start)) 
