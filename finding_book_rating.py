7d2aa1433dad6f430a3cc1d1b3d05c6dc6ef2619
pratheek <ms.pratheek1@gmail.com>
2020-01-10 19:43:42 +0530

    good reads link extraction

--- finding_book_rating.py
+++ finding_book_rating.py
@@ -19,16 +19,20 @@ wb_obj=openpyxl.load_workbook(path)
 sheet_obj=wb_obj.active
 m_row=sheet_obj.max_row
 print("rows="+str(m_row))
+'''
 for i in range(1,m_row+1):
     bookname=sheet_obj.cell(row=i,column=2).value
     bookauthor=sheet_obj.cell(row=i,column=3).value
     #searchPhrase=bookname+" by "+bookauthor+" in good reads"
     searchPhrase="Catch-22 by Joseph Heller in good reads"
     print(searchPhrase)
+'''
      
-  
+searchPhrase="Catch-22 by Joseph Heller in good reads"  
 for j in search(searchPhrase, tld="com", num=10, stop=1, pause=3): 
-    print(j)
+    goodreadsurl=j
+    print(goodreadsurl)
+
 
     
 end = time.time()
