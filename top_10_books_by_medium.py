fae129bf5ad5e517840541015fb4cecc68a12759
pratheek <ms.pratheek1@gmail.com>
2020-01-10 16:23:29 +0530

    excel file partially working

--- top_10_books_by_medium.py
+++ top_10_books_by_medium.py
@@ -13,6 +13,10 @@ path="C:\\Users\\pratms\\OneDrive - Capgemini\\Python Scripts\\excel_doc.xlsx"
 wb_obj=openpyxl.load_workbook(path)
 sheet_obj=wb_obj.active
 m_row=sheet_obj.max_row
+
+wb_objex = openpyxl.Workbook()
+sheet_objex = wb_objex.active
+
 finalResult=[]
 bookdict={}
 bookcount=1
@@ -45,8 +49,19 @@ for bn,ba in bookdict.items():
     f = open(r"C:\Users\pratms\pythonprojects\webscrapping\webscrapping_medium_top_10\top100BokksByMedium.txt", "a")
     f.write(str(booknum)+'\t'+str(bookname)+'\t'+bookauthor+'\n')
     f.close()
+    sheet_objex.cell(row=1,column=1).value='Sl. No'
+    sheet_objex.cell(row=1,column=2).value='Book Name'
+    sheet_objex.cell(row=1,column=3).value='Book Author'
+    sheet_objex.cell(row=1,column=4).value='Rating'
+    wb_objex.save("C:\\Users\\pratms\\OneDrive - Capgemini\\Python Scripts\\excel_doc1.xlsx")
+    
 ###########################################################################
-       
+
+for i in range(1,m_row+1):
+    booknumex=str(sheet_obj.cell(row=i,column=1).value)
+    booknameex=str(sheet_obj.cell(row=i,column=2).value)
+    bookauthorx=str(sheet_obj.cell(row=i,column=3).value)
+    print(booknumex,booknameex,"by",bookauthorx)     
 
     
 end = time.time()
