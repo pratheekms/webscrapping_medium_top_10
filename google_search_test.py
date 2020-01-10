c7a2fdbc825eb0fc544da03b01fa03b9efea7f60
pratheek <ms.pratheek1@gmail.com>
2020-01-10 20:59:40 +0530

    rating complete

--- google_search_test.py
+++ google_search_test.py
@@ -4,14 +4,26 @@ Created on Fri Jan 10 19:05:37 2020
 
 @author: pratms
 """
-
+import bs4,requests
+#import lxml
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 try: 
     from googlesearch import search 
 except ImportError:  
     print("No module named 'google' found") 
   
 # to search 
-query = "The Unbearable Lightness of Being by Milan Kundera good reads"
-  
-for j in search(query, tld="com", num=10, stop=1, pause=3): 
-    print(j)
+searchPhrase="1984 by George Orwell in goodreads" 
+print(searchPhrase) 
+for j in search(searchPhrase, tld="com", num=10, stop=1, pause=3): 
+    goodreadsurl=j
+    print(goodreadsurl)
+print("bs4 starting")
+res=requests.get(goodreadsurl,verify=False)
+res.raise_for_status()
+soup=bs4.BeautifulSoup(res.text,'lxml')
+#rating=soup.find_all("span",attrs={"itemprop":"ratingValue"})
+ratingObj=soup.find("span", itemprop="ratingValue")
+rating=ratingObj.getText()
+print(rating)
