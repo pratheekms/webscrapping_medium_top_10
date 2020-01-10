550fcc0ce158113d68fd910201d5615b961b394f
pratheek <ms.pratheek1@gmail.com>
2020-01-11 02:00:22 +0530

    test 1

--- top_10_books_by_medium.py
+++ top_10_books_by_medium.py
@@ -11,10 +11,11 @@ import openpyxl
 import random
 import logging
 
-GlobalPath=r"C:\Users\pratms\pythonprojects\webscrapping\webscrapping_medium_top_10"
+from datetime import datetime
 
-newfile=GlobalPath+'\\'+str(time.ctime(time.time()))+'.log'
-logging.basicConfig(filename=newfile, 
+GlobalPath=r"C:\Users\prath\pythonproj\webscrapping_medium_top_10"
+LOG_FILENAME = datetime.now().strftime('logfile_%H_%M_%S_%d_%m_%Y.log')
+logging.basicConfig(filename=LOG_FILENAME, 
                     format='%(asctime)s %(message)s', 
                     filemode='w')
 
@@ -23,20 +24,22 @@ logger=logging.getLogger()
   
 #Setting the threshold of logger to DEBUG 
 logger.setLevel(logging.DEBUG) 
-  
+'''  
 #Test messages 
 logger.debug("Harmless debug Message") 
 logger.info("Just an information") 
 logger.warning("Its a Warning") 
 logger.error("Did you try to divide by zero") 
 logger.critical("Internet is down")
-
+'''
 
 
 #path=GlobalPath+r"\excel_doc1.xlsx"
 #open excel file code start
 print("Global path is",GlobalPath)
+logger.info("Global path is",GlobalPath)
 print("opening new excel for book author rating")
+logger.info("opening new excel for book author rating")
 wb_objex = openpyxl.Workbook()
 sheet_objex = wb_objex.active
 sheet_objex.cell(row=1,column=1).value='Sl. No'
@@ -45,11 +48,13 @@ sheet_objex.cell(row=1,column=3).value='Book Author'
 sheet_objex.cell(row=1,column=4).value='Rating'
 
 print("writing of headers to the new excel sheet")
+logger.info("writing of headers to the new excel sheet")
 
 finalResult=[]
 bookdict={}
 bookcount=1
-print("scraping web for top 100 books start")    
+print("scraping web for top 100 books start") 
+logger.info("------scraping web for top 100 books start------")   
 url="https://medium.com/world-literature/creating-the-ultimate-list-100-books-to-read-before-you-die-45f1b722b2e5"
 res=requests.get(url,verify=False)
 res.raise_for_status()
@@ -58,28 +63,38 @@ soup=bs4.BeautifulSoup(res.text,'lxml')
 bookAndAuthors=soup.find_all("strong",attrs={"class":"id ke"})
 
 
-print("scraping web for top 100 books end")
+print("------scraping web for top 100 books end------")
+logger.info("------scraping web for top 100 books end------")
 
 print("exctrating data into dict start")
+logger.info("exctrating data into dict start")
 
 for i in range(0,len(bookAndAuthors)-4):
     if len(bookAndAuthors[i].getText())>2:
         finalResult.append(bookAndAuthors[i].getText())
         print(bookAndAuthors[i].getText())
+        
 
 for i in finalResult:
     print("book "+str(bookcount)+i)
+    logger.info("book "+str(bookcount)+i)
     print(len(finalResult[bookcount-1]))
+   
     bookdict.update({bookcount:{'name':i.split(' by ')[0],'author':i.split(' by ')[1]}})
     bookcount+=1
 print("exctrating data into dict end")
+logger.info("exctrating data into dict end")
 print("exctrating from dict to list")
-print("writng list slno book author to excel start")
+logger.info("exctrating from dict to list")
+print("------writng list slno book author to excel start------")
+logger.info("------writng list slno book author to excel start------")
 for bn,ba in bookdict.items():
     print("book num",bn)
+    
     booknum=bn
     for key in ba:
         print(key+":",ba[key])
+        
         bookname=ba['name']
         bookauthor=ba['author']
     f = open(GlobalPath+r"\top100BokksByMedium.txt", "a")
@@ -89,10 +104,13 @@ for bn,ba in bookdict.items():
     sheet_objex.cell(row=booknum+1,column=2).value=str(bookname)
     sheet_objex.cell(row=booknum+1,column=3).value=str(bookauthor)
     #sheet_objex.cell(row=booknum+1,column=4).value='Rating'
-
+print("------writng list slno book author to excel end------")
+logger.info("------writng list slno book author to excel end------")
 wb_objex.save(GlobalPath+r"\top100BokksByMedium.xlsx")
-print("excel with with slno book author saved")
-print("rating process starts")
+print("------excel with with slno book author saved------")
+logger.info("------excel with with slno book author saved------")
+print("------------rating process starts------------")
+logger.info("------------rating process starts------------")
 #book name and book author details are saved to an excel file
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -100,42 +118,63 @@ try:
     from googlesearch import search 
 except ImportError:  
     print("No module named 'google' found") 
+    logger.info("Just an information")
 
 
 pathForRating=GlobalPath+r"\top100BokksByMedium.xlsx"
-print("saved excel file open")
+print("------saved excel file open------")
+logger.info("------saved excel file open------")
 wb_obj=openpyxl.load_workbook(pathForRating)
 sheet_obj=wb_obj.active
 m_row=sheet_obj.max_row
 print("rows="+str(m_row))
-for i in range(1,m_row+1):
+logger.info("------total rows in excel file="+str(m_row),"------")
+logger.info("---------------------------------------------------")
+for i in range(1,m_row):
+    logger.info("---------------------------------------------------")
     bookname=sheet_obj.cell(row=i+1,column=2).value
     bookauthor=sheet_obj.cell(row=i+1,column=3).value
     searchPhrase=bookname+" by "+bookauthor+" goodreads"
     #searchPhrase="Catch-22 by Joseph Heller in good reads"
     print("modified search phrase ",searchPhrase)
+    logger.info("book num=",i,'\t',bookname,'\t',bookauthor)
+    logger.info("modified search phrase --->",searchPhrase)
 #searchPhrase="1984 by George Orwell in goodreads" 
     #print(searchPhrase) 
     for j in search(searchPhrase, tld="com", num=10, stop=1, pause=random.randint(1,20)): 
         goodreadsurl=j
     print("good reads URL for ",bookname,bookauthor,"is ",goodreadsurl)
+    logger.info("good reads URL for ",bookname,bookauthor,"is --->",goodreadsurl)
     
-    print("scrapping for rating start")
+    print("---scrapping for rating start---")
+    logger.info("---scrapping for rating start---")
     res=requests.get(goodreadsurl,verify=False)
     res.raise_for_status()
     soup=bs4.BeautifulSoup(res.text,'lxml')
     ratingObj=soup.find("span", itemprop="ratingValue")
-    rating=ratingObj.getText()
-    print("ratinng of",bookname,bookauthor, rating)
-    sheet_obj.cell(row=i+1,column=4).value=str(rating)
-    print("rating write complete")
-wb_obj.save(pathForRating)
-print("final excel saved")
-
-
-   
+    try:
+        rating=ratingObj.getText()
+        print("ratinng of",bookname,bookauthor, rating)
+        logger.info("ratinng of",bookname,bookauthor, rating)
+        sheet_obj.cell(row=i+1,column=4).value=str(rating)
+        print("---rating write complete---")
+        logger.info("---rating write complete---")
+        
+    except:
+        print("ratinng of",bookname,bookauthor,"Error")
+        logger.info("ratinng of",bookname,bookauthor,"Error")
+        sheet_obj.cell(row=i+1,column=4).value="Error"
+        print("rating write complete with ERROR")
+        logger.info("^^^rating write complete with ERROR^^^")
+        print("-----------------------")
+        wb_obj.save(pathForRating)
+        print("final excel saved")
+        logger.info("---final excel saved---")
+        logger.info("---------------------------------------------------")
+  
 #wb_objex.save(r"C:\Users\pratms\pythonprojects\webscrapping\webscrapping_medium_top_10\top100BokksByMedium.xlsx")
    
 end = time.time()
 print("time taken by program is:"+str(end - start)) 
+logger.info("____time taken by program is:"+str(end - start)+"____")
 logging.shutdown()
